766cd9f3f5c43de1c5ee4584a3586e7442699670
weili <willyykidd@gmail.com>
2025-04-20 15:07:13 -0400

    ugly fix

--- test.py
+++ test.py
@@ -3,17 +3,24 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from config import *
+from config import TEST_PATH
 from dataset import ArxivDataset
 from model import ArxivClassifier
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+train_dataset = ArxivDataset(TRAIN_PATH)
 test_dataset = ArxivDataset(TEST_PATH)
+test_dataset.tokenizer = train_dataset.tokenizer
+test_dataset.encoded = [
+    test_dataset.tokenizer.encode(text) for text in test_dataset.texts
+]
+test_dataset.padded = test_dataset.tokenizer.pad_batch(test_dataset.encoded)
 test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
 model = ArxivClassifier(
-    vocab_size=len(test_dataset.tokenizer.vocab),
-    num_classes=len(test_dataset.labels),
+    vocab_size=len(train_dataset.tokenizer.vocab),
+    num_classes=len(train_dataset.labels),
 ).to(device)
 
 model.load_state_dict(torch.load("model.pt"))
